b5b405b931b0bbb5917c574b2db26602917a3149
Jamie (Lexteam) <lexwaresupport@outlook.com>
2014-09-06 22:40:13 +0100

    Added copyright notices.

--- versions/v1.php
+++ versions/v1.php
@@ -1,4 +1,20 @@
 <?php
+	/*
+	 * Copyright 2014 Lexteam (https://github.com/Lexteam)
+	 * 
+	 * Licensed under the Apache License, Version 2.0 (the "License");
+     * you may not use this file except in compliance with the License.
+     * You may obtain a copy of the License at
+	 * 
+     *   http://www.apache.org/licenses/LICENSE-2.0
+	 * 
+     * Unless required by applicable law or agreed to in writing, software
+     * distributed under the License is distributed on an "AS IS" BASIS,
+     * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+     * See the License for the specific language governing permissions and
+     * limitations under the License.
+	*/
+	
 	// methods
 	function getPack($arguments, $api_version, $api_versions) { // actually pack, but since there is a function in php called pack
 		if(count($arguments) == 1 || count($arguments) == 2) {
